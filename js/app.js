da66edccf22aa001f755519ff46847b0414a2cc1
IosifBonda <bondaiosiftimotei@gmail.com>
2026-07-18 19:52:40 +0300

    Prima versiune a site-ului

---
+++ js/app.js
@@ -0,0 +1,74 @@
+
+console.log("JavaScript funcționează!");
+
+const menuButton = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+
+    nav.classList.toggle("open");
+
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const top = section.offsetTop - 150;
+
+        const height = section.offsetHeight;
+
+        if (window.scrollY >= top) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        nav.classList.remove("open");
+
+    });
+
+});
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
+
+console.log("EVRIKA.");
